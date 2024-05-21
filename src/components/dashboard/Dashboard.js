8d2cc006773af5cf82ebf0431e0b6eb1b1712483
Joseph Flores <smile.jf.871@gmail.com>
2024-05-21 05:10:55 -0700

    Updated Filter section
    Unable to find a proper filter-emoji, so used something that looks close to one. There isn't any within the HTML W3 website

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,7 +4,57 @@ import "./Dashboard.css";
 function Dashboard() {
   return (
     <div id="dashboard">
-      <div id="dashboard-form-container">This is where the form goes</div>
+      <div id="dashboard-form-container">
+        <h1 id="form-title">&#9067; Filter</h1>
+        <form class="filter-category">
+          <label class="filter-section">Mode of Instruction</label><br></br>
+          <input type="checkbox" class="filter-option" name="instruction-mode" value="online"></input>
+          <span class="filter-text">Online</span><br></br>
+          <input type="checkbox" class="filter-option" name="instruction-mode" value="in-person"></input>
+          <span class="filter-text">In-Person</span><br></br>
+        </form>
+        <form class="filter-category">
+          <label class="filter-section">Subject</label><br></br>
+          <input type="checkbox" class="filter-option" name="subject" value="math"></input>
+          <span class="filter-text">Math</span><br></br>
+          <input type="checkbox" class="filter-option" name="subject" value="physics"></input>
+          <span class="filter-text">Physics</span><br></br>
+          <input type="checkbox" class="filter-option" name="subject" value="history"></input>
+          <span class="filter-text">History</span><br></br>
+        </form>
+        <form class="filter-category">
+          <label class="filter-section">Qualifications</label><br></br>
+          <input type="checkbox" class="filter-option" name="qualifications" value="high-school"></input>
+          <span class="filter-text">High School Disploma</span><br></br>
+          <input type="checkbox" class="filter-option" name="qualifications" value="bachelors"></input>
+          <span class="filter-text">Bachelor's Degree</span><br></br>
+          <input type="checkbox" class="filter-option" name="qualifications" value="masters"></input>
+          <span class="filter-text">Master's Degree</span><br></br>
+          <input type="checkbox" class="filter-option" name="qualifications" value="doctors"></input>
+          <span class="filter-text">PhD</span><br></br>
+        </form>
+        <form class="filter-category">
+          <label class="filter-section">UC Campus</label><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="riverside"></input>
+          <span class="filter-text">Riverside</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="berkely"></input>
+          <span class="filter-text">Berkely</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="los-angeles"></input>
+          <span class="filter-text">Los Angeles</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="san-diego"></input>
+          <span class="filter-text">San Diego</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="irvine"></input>
+          <span class="filter-text">Irvine</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="davis"></input>
+          <span class="filter-text">Davis</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="merced"></input>
+          <span class="filter-text">Merced</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="santa-barbara"></input>
+          <span class="filter-text">Santa Barbara</span><br></br>
+          <input type="checkbox" class="filter-option" name="campus" value="santa-cruz"></input>
+          <span class="filter-text">Santa Cruz</span><br></br>
+        </form>
+      </div>
       <div id="dashboard-student-list-container">
         <h1>Recommended Students</h1>
         <div id="student-list">
