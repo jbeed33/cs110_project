b6a284383c1eed9ac14fa902b41a506c94ee0127
Joshua Beed <70536517+jbeed33@users.noreply.github.com>
2024-05-23 19:53:38 -0700

    added try catch to all admin routes

--- backend/routes/AdminRoutes.js
+++ backend/routes/AdminRoutes.js
@@ -4,41 +4,58 @@ let adminController = require("../controllers/AdminController");
 
 let router = express.Router();
 
-//Admin can get all users.
+// Admin can get all users.
 router.get("/", async (req, res) => {
-  // TO DO: cookies will be used for authentication
-
-  let user = await adminController.getAllUsers();
-  if (user === null) {
+  try {
+    // TO DO: cookies will be used for authentication
+    let user = await adminController.getAllUsers();
+    if (user === null) {
+      return res
+        .status(404)
+        .json({ message: "Could not retrieve any users. Please try again" });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
     return res
-      .status(404)
-      .json({ message: "could not retrieve any users. Please try again" });
+      .status(500)
+      .json({ message: "An error occurred. Please try again" });
   }
-  return res.status(200).json(user);
 });
 
-//Admin can delete a user.
+// Admin can delete a user.
 router.delete("/:userId", async (req, res) => {
-  let userId = req.params.userId;
-  console.log(userId);
-  let user = await userController.deleteUser(userId);
-  if (user === null) {
+  try {
+    let userId = req.params.userId;
+    console.log(userId);
+    let user = await userController.deleteUser(userId);
+    if (user === null) {
+      return res
+        .status(404)
+        .json({ message: "Could not delete user. Please try again" });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
     return res
-      .status(404)
-      .json({ message: "could not delete user. Please try again" });
+      .status(500)
+      .json({ message: "An error occurred. Please try again" });
   }
-  return res.status(200).json(user);
 });
 
-//Admin can edit a user.
+// Admin can edit a user.
 router.put("/:userId", async (req, res) => {
-  let userId = req.params.userId;
-  result = await userController.updateUser(userId, req.body);
-  if (result === null || result === undefined) {
-    return res.status(404).send("Could not update user. Please try again");
+  try {
+    let userId = req.params.userId;
+    let result = await userController.updateUser(userId, req.body);
+    if (result === null || result === undefined) {
+      return res.status(404).send("Could not update user. Please try again");
+    }
+    return res.status(200).send(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("An error occurred. Please try again");
   }
-
-  return res.status(200).send(result);
 });
 
 module.exports = router;
