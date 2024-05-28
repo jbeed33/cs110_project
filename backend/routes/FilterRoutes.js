0824f772576dcd0137b6218795f37fd13d0c0137
Joshua Beed <70536517+jbeed33@users.noreply.github.com>
2024-05-27 22:22:29 -0700

    quick updated to filter route. Now the filter route only works when a filter is applied.

--- backend/routes/FilterRoutes.js
+++ backend/routes/FilterRoutes.js
@@ -4,6 +4,13 @@ let filterController = require("../controllers/FilterController");
 let router = express.Router();
 
 router.get("/", async (req, res) => {
+  //Must have filters added to use this route.
+  if (Object.keys(req.query).length === 0) {
+    return res
+      .status(200)
+      .send({ message: "Please add a filter to use this route." });
+  }
+
   try {
     const filters = req.query;
     console.log(filters);
