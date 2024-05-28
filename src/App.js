58980d08458d07eb2bf726a52c4b0b2690fa1833
Aidan Ching <achin053@ucr.edu>
2024-05-28 03:13:19 -0700

    added admin panel functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import Login from './components/login/Login';
+import AdminPanel from "./components/admin/AdminPanel";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      {/*<Navbar /> */}
-      {/*<Dashboard />*/}
+      {/* <Login/> */}
+      <Navbar />
+      {/* <Dashboard /> */}
+      <AdminPanel/>
     </div>
   );
 }
