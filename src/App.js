bf52ccc70a35acffcaba2184a0a598eb32442ddb
Jason Guan <jasonguan067@gmail.com>
2024-05-28 04:33:19 -0700

    finished messages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import Login from './components/login/Login';
+import Messages from "./components/messages/Messages";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      {/*<Navbar /> */}
-      {/*<Dashboard />*/}
+      {/*<Login/> */}
+      <Navbar />
+      <Messages/>
+      {/* <Dashboard /> */}
     </div>
   );
 }
