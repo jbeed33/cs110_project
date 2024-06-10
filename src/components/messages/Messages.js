e16e516fade9def1957d9b9586ec657b672f45eb
Jason Guan <jasonguan067@gmail.com>
2024-06-10 08:36:47 -0700

    UI improvements

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Messages.css";
+import Navbar from "../navbar/Navbar";
 
-const API_URL = "http://localhost:8080"; 
+const API_URL = "http://localhost:8080";
 
 const formatDate = (dateString) => {
   const date = new Date(dateString);
@@ -19,7 +20,7 @@ const formatDate = (dateString) => {
   }
 };
 
-function Messages() {
+function Messages({ currentUserId }) {
   const [contacts, setContacts] = useState([]);
   const [messages, setMessages] = useState([]);
   const [selectedContact, setSelectedContact] = useState(null);
@@ -30,69 +31,57 @@ function Messages() {
   useEffect(() => {
     axios
       .get(`${API_URL}/api/chat/group`, { withCredentials: true })
-      .then(response => {
-        console.log(response.data.userGroups);
-        const contactsData = response.data.userGroups.map(group => {
-          // Check if group.lastMessage exists and has publishDate
-          if (group.lastMessage && group.lastMessage.publishDate) {
-            return {
-              userId: group.chatPartner.userId,
-              name: group.chatPartner.userName,
-              image: group.chatPartner.userImage,
-              lastMessageDate: group.lastMessage.publishDate,
-              lastMessageText: group.lastMessage.message,
-              chatId: group.chatId
-            };
-          } else {
-            // Handle the case where lastMessage or publishDate is missing
-            // You can set default values or handle it as you need
-            return {
-              userId: group.chatPartner.userId,
-              name: group.chatPartner.userName,
-              image: group.chatPartner.userImage,
-              lastMessageDate: "", // Set default value for lastMessageDate
-              lastMessageText: "", // Set default value for lastMessageText
-              chatId: group.chatId
-            };
-          }
+      .then((response) => {
+        const contactsData = response.data.userGroups.map((group) => {
+          return {
+            userId: group.chatPartner.userId,
+            name: group.chatPartner.userName,
+            image: group.chatPartner.userImage,
+            lastMessageDate: group.lastMessage?.publishDate || "",
+            lastMessageText: group.lastMessage?.message || "",
+            chatId: group.chatId,
+          };
         });
-        console.log("contactsData", contactsData);
         setContacts(contactsData);
         setSelectedContact(contactsData[0]);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
-
   useEffect(() => {
     const intervalId = setInterval(() => {
-      console.log("getMessage useEffect");
       if (currentGroup) {
-        axios.get(`${API_URL}/api/chat/messages/${currentGroup}`, { withCredentials: true })
+        axios
+          .get(`${API_URL}/api/chat/messages/${currentGroup}`, {
+            withCredentials: true,
+          })
           .then((response) => {
-            console.log("getting messages", response.data);
+            console.log("Messages fetched: ", response.data);
             setMessages(response.data);
           });
       }
     }, 3000);
-  
-    
+
     return () => clearInterval(intervalId);
   }, [currentGroup]);
- 
+
   const handleSelectContact = (e) => {
     const receiver = e.target.dataset.chatid || null;
-    console.log("receiver", receiver);
+    const contact = contacts.find((contact) => contact.chatId === receiver);
+    console.log("Selected contact:", contact);
     setCurrentChat(receiver);
-    // setSelectedContact(contact);
+    setSelectedContact(contact);
+
     if (receiver) {
       axios
-        .get(`${API_URL}/api/chat/messages/${receiver}`, { withCredentials: true })
+        .get(`${API_URL}/api/chat/messages/${receiver}`, {
+          withCredentials: true,
+        })
         .then((response) => {
+          console.log("Messages for selected contact: ", response.data);
           setCurrentGroup(receiver);
-          console.log(response.data);
           setMessages(response.data);
         });
     }
@@ -100,18 +89,15 @@ function Messages() {
 
   const handleSendMessage = (newMessage) => {
     if (newMessage.trim() === "") return;
-    console.log("qweo", selectedContact);
     const newMsg = {
       receiver: selectedContact.userId,
       message: newMessage,
     };
-    console.log(currentChat);
-  
-    // Include credentials in the request headers
     axios
-      .post(`${API_URL}/api/chat/messages/${currentChat}`, newMsg, { withCredentials: true })
+      .post(`${API_URL}/api/chat/messages/${currentChat}`, newMsg, {
+        withCredentials: true,
+      })
       .then((response) => {
-        console.log(response.data);
         setMessages((prevMessages) => [...prevMessages, response.data]);
         setNewMessage("");
       })
@@ -119,73 +105,90 @@ function Messages() {
         console.error("Error sending message:", error);
       });
   };
+
   return (
-    <div id="messages">
-      <div id="messages-form-container">
-        <h1 id="form-title">&#9067; Messages</h1>
-        <div id="sidebar">
-          {contacts.map((contact) => (
-            <div
-              key={contact.userId}
-              data-chatid = {contact.chatId}
-              onClick = {e => handleSelectContact(e)}
-              className={`contact ${
-                selectedContact && selectedContact.userId === contact.userId
-                  ? "selected"
-                  : ""
-              }`}
-              // onClick={() => handleSelectContact(contact)}
-            >
-              <div className="contact-avatar">
-                {contact.image ? (
-                  <img src={contact.image} alt={`${contact.name}'s avatar`} />
-                ) : (
-                  <div className="default-avatar">{contact.name[0]}</div>
-                )}
-              </div>
-              <div className="contact-info">
-                <div className="contact-name">{contact.name}</div>
-              </div>
-              <div className="contact-date">
-                {formatDate(contact.lastMessageDate)}
-              </div>
-              <div className="last-message-text">{contact.lastMessageText}</div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div id="chat-header">
-        Conversation with {selectedContact && selectedContact.name}
-      </div>
-      <div id="chatbox-container">
-        <div id="chat-window">
-          <div id="chat-messages">
-            {messages.map((message, index) => (
+    <>
+      <Navbar />
+      <div id="messages">
+        <div id="messages-form-container">
+          <h1 id="form-title">&#9067; Messages</h1>
+          <div id="sidebar">
+            {contacts.map((contact) => (
               <div
-                key={index}
-                className={`message ${
-                  message.sender === "encryptedEmailToken12345" ? "sent" : "received"
+                key={contact.userId}
+                data-chatid={contact.chatId}
+                onClick={(e) => handleSelectContact(e)}
+                className={`contact ${
+                  selectedContact && selectedContact.userId === contact.userId
+                    ? "selected"
+                    : ""
                 }`}
               >
-                {message.sender !== "encryptedEmailToken12345" && (
-                  <div className="message-avatar"></div>
-                )}
-                <div className="message-content">{message.message}</div>
+                <div className="contact-avatar">
+                  {contact.image ? (
+                    <img src={contact.image} alt={`${contact.name}'s avatar`} />
+                  ) : (
+                    <div className="default-avatar">{contact.name[0]}</div>
+                  )}
+                </div>
+                <div className="contact-info">
+                  <div className="contact-name">{contact.name}</div>
+                </div>
+                <div className="last-message-text">
+                  {contact.lastMessageText}
+                </div>
+                <div className="contact-date">
+                  {formatDate(contact.lastMessageDate)}
+                </div>
               </div>
             ))}
           </div>
-          <div id="chat-input">
-            <input
-              type="text"
-              placeholder="New message"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-            />
-            <button onClick={() => handleSendMessage(newMessage)}>SEND</button>
+        </div>
+        <div id="chat-header">
+          Conversation with {selectedContact && selectedContact.name}
+        </div>
+        <div id="chatbox-container">
+          <div id="chat-window">
+            <div id="chat-messages">
+              {messages.map((message, index) => {
+                console.log("Message sender:", message.sender);
+                console.log("Current User ID:", currentUserId);
+                console.log("Selected contact ID:", selectedContact?.userId);
+                return (
+                  <div key={index}
+                    className={`message ${
+                      message.sender === null
+                        ? "received"
+                        : "sent"
+                    }`}
+                  >
+                    {message.sender === null && (
+                      <div className="message-avatar">
+                        <div className="message-avatar-text">
+                          {selectedContact.name[0]}
+                        </div>
+                      </div>
+                    )}
+                    <div className="message-content">{message.message}</div>
+                  </div>
+                );
+              })}
+            </div>
+            <div id="chat-input">
+              <input
+                type="text"
+                placeholder="New message"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+              />
+              <button onClick={() => handleSendMessage(newMessage)}>
+                SEND
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
